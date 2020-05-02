0c4d6db2a89b5a0d462f1db01ff1e347c6747781
jhamPac <cloud@kevia.me>
2020-05-02 10:55:10 -0700

    func for creating a bot

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"context"
+	"log"
+	"os"
 	"regexp"
 	"sync"
 
 	"github.com/dghubble/go-twitter/twitter"
-	"github.com/ipfs/ipfs-cluster/api/rest/client"
+	"github.com/dghubble/oauth1"
+	ipfsCluster "github.com/ipfs/ipfs-cluster/api/rest/client"
+	"github.com/joho/godotenv"
 )
 
 // Action that serves as a command for ipfs cluster
@@ -28,16 +32,34 @@ type Bot struct {
 	name          string
 	id            string
 	twClient      *twitter.Client
-	clusterClient client.Client
+	clusterClient ipfsCluster.Client
 	followedBy    sync.Map
 	die           chan struct{}
 }
 
 // New creates Titanium Giant
 func New() (*Bot, error) {
-	return nil, nil
+	ctx, cancel := context.WithCancel(context.Background())
+
+	config := oauth1.NewConfig(os.Getenv("APIKEY"), os.Getenv("APISECRET"))
+	token := oauth1.NewToken(os.Getenv("TOKEN"), os.Getenv("TOKENSECRET"))
+	httpClient := config.Client(ctx, token)
+	twClient := twitter.NewClient(httpClient)
+
+	bot := &Bot{
+		ctx:      ctx,
+		cancel:   cancel,
+		twClient: twClient,
+		die:      make(chan struct{}, 1),
+	}
+
+	return bot, nil
 }
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Could not load the environment variables")
+	}
 
 }
